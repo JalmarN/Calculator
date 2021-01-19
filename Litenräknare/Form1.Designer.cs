a172bdb9a14bef55c9c3f41d04518e1be9d26486
JalmarN <74744872+JalmarN@users.noreply.github.com>
2021-01-19 15:17:25 +0100

    Add files via upload

---
+++ Litenräknare/Form1.Designer.cs
@@ -0,0 +1,413 @@
+﻿namespace LitenRäknare
+{
+    partial class Liten_Räknare
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Ett = new System.Windows.Forms.Button();
+            this.Två = new System.Windows.Forms.Button();
+            this.Tre = new System.Windows.Forms.Button();
+            this.Fyra = new System.Windows.Forms.Button();
+            this.Fem = new System.Windows.Forms.Button();
+            this.Sex = new System.Windows.Forms.Button();
+            this.Sju = new System.Windows.Forms.Button();
+            this.Åtta = new System.Windows.Forms.Button();
+            this.Nio = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.Noll = new System.Windows.Forms.Button();
+            this.Resultat = new System.Windows.Forms.TextBox();
+            this.C = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
+            this.button21 = new System.Windows.Forms.Button();
+            this.Likamed = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Ett
+            // 
+            this.Ett.BackColor = System.Drawing.Color.DimGray;
+            this.Ett.ForeColor = System.Drawing.Color.Snow;
+            this.Ett.Location = new System.Drawing.Point(12, 451);
+            this.Ett.Name = "Ett";
+            this.Ett.Size = new System.Drawing.Size(75, 75);
+            this.Ett.TabIndex = 0;
+            this.Ett.Text = "1";
+            this.Ett.UseVisualStyleBackColor = false;
+            this.Ett.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Två
+            // 
+            this.Två.BackColor = System.Drawing.Color.DimGray;
+            this.Två.ForeColor = System.Drawing.Color.Snow;
+            this.Två.Location = new System.Drawing.Point(93, 451);
+            this.Två.Name = "Två";
+            this.Två.Size = new System.Drawing.Size(75, 75);
+            this.Två.TabIndex = 1;
+            this.Två.Text = "2";
+            this.Två.UseVisualStyleBackColor = false;
+            this.Två.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Tre
+            // 
+            this.Tre.BackColor = System.Drawing.Color.DimGray;
+            this.Tre.ForeColor = System.Drawing.Color.Snow;
+            this.Tre.Location = new System.Drawing.Point(174, 451);
+            this.Tre.Name = "Tre";
+            this.Tre.Size = new System.Drawing.Size(75, 75);
+            this.Tre.TabIndex = 2;
+            this.Tre.Text = "3";
+            this.Tre.UseVisualStyleBackColor = false;
+            this.Tre.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Fyra
+            // 
+            this.Fyra.BackColor = System.Drawing.Color.DimGray;
+            this.Fyra.ForeColor = System.Drawing.Color.Snow;
+            this.Fyra.Location = new System.Drawing.Point(12, 370);
+            this.Fyra.Name = "Fyra";
+            this.Fyra.Size = new System.Drawing.Size(75, 75);
+            this.Fyra.TabIndex = 3;
+            this.Fyra.Text = "4";
+            this.Fyra.UseVisualStyleBackColor = false;
+            this.Fyra.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Fem
+            // 
+            this.Fem.BackColor = System.Drawing.Color.DimGray;
+            this.Fem.ForeColor = System.Drawing.Color.Snow;
+            this.Fem.Location = new System.Drawing.Point(93, 370);
+            this.Fem.Name = "Fem";
+            this.Fem.Size = new System.Drawing.Size(75, 75);
+            this.Fem.TabIndex = 4;
+            this.Fem.Text = "5";
+            this.Fem.UseVisualStyleBackColor = false;
+            this.Fem.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Sex
+            // 
+            this.Sex.BackColor = System.Drawing.Color.DimGray;
+            this.Sex.ForeColor = System.Drawing.Color.Snow;
+            this.Sex.Location = new System.Drawing.Point(174, 370);
+            this.Sex.Name = "Sex";
+            this.Sex.Size = new System.Drawing.Size(75, 75);
+            this.Sex.TabIndex = 5;
+            this.Sex.Text = "6";
+            this.Sex.UseVisualStyleBackColor = false;
+            this.Sex.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Sju
+            // 
+            this.Sju.BackColor = System.Drawing.Color.DimGray;
+            this.Sju.ForeColor = System.Drawing.Color.Snow;
+            this.Sju.Location = new System.Drawing.Point(12, 289);
+            this.Sju.Name = "Sju";
+            this.Sju.Size = new System.Drawing.Size(75, 75);
+            this.Sju.TabIndex = 6;
+            this.Sju.Text = "7";
+            this.Sju.UseVisualStyleBackColor = false;
+            this.Sju.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Åtta
+            // 
+            this.Åtta.BackColor = System.Drawing.Color.DimGray;
+            this.Åtta.ForeColor = System.Drawing.Color.Snow;
+            this.Åtta.Location = new System.Drawing.Point(93, 289);
+            this.Åtta.Name = "Åtta";
+            this.Åtta.Size = new System.Drawing.Size(75, 75);
+            this.Åtta.TabIndex = 7;
+            this.Åtta.Text = "8";
+            this.Åtta.UseVisualStyleBackColor = false;
+            this.Åtta.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Nio
+            // 
+            this.Nio.BackColor = System.Drawing.Color.DimGray;
+            this.Nio.ForeColor = System.Drawing.Color.Snow;
+            this.Nio.Location = new System.Drawing.Point(174, 288);
+            this.Nio.Name = "Nio";
+            this.Nio.Size = new System.Drawing.Size(75, 75);
+            this.Nio.TabIndex = 8;
+            this.Nio.Text = "9";
+            this.Nio.UseVisualStyleBackColor = false;
+            this.Nio.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.DimGray;
+            this.button10.ForeColor = System.Drawing.Color.Snow;
+            this.button10.Location = new System.Drawing.Point(266, 532);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 75);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "+";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.symbol_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.DimGray;
+            this.button11.ForeColor = System.Drawing.Color.Snow;
+            this.button11.Location = new System.Drawing.Point(266, 451);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 75);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "-";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.symbol_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.DimGray;
+            this.button12.ForeColor = System.Drawing.Color.Snow;
+            this.button12.Location = new System.Drawing.Point(266, 370);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 75);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "x";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.symbol_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.DimGray;
+            this.button13.ForeColor = System.Drawing.Color.Snow;
+            this.button13.Location = new System.Drawing.Point(266, 289);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 75);
+            this.button13.TabIndex = 12;
+            this.button13.Text = "/";
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.symbol_Click);
+            // 
+            // Noll
+            // 
+            this.Noll.BackColor = System.Drawing.Color.DimGray;
+            this.Noll.ForeColor = System.Drawing.Color.Snow;
+            this.Noll.Location = new System.Drawing.Point(93, 532);
+            this.Noll.Name = "Noll";
+            this.Noll.Size = new System.Drawing.Size(75, 75);
+            this.Noll.TabIndex = 13;
+            this.Noll.Text = "0";
+            this.Noll.UseVisualStyleBackColor = false;
+            this.Noll.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Resultat
+            // 
+            this.Resultat.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Resultat.Location = new System.Drawing.Point(12, 12);
+            this.Resultat.Name = "Resultat";
+            this.Resultat.Size = new System.Drawing.Size(329, 75);
+            this.Resultat.TabIndex = 14;
+            this.Resultat.Text = "0";
+            this.Resultat.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.Resultat.TextChanged += new System.EventHandler(this.Resultat_TextChanged);
+            // 
+            // C
+            // 
+            this.C.BackColor = System.Drawing.Color.DimGray;
+            this.C.Font = new System.Drawing.Font("Microsoft Sans Serif", 5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C.ForeColor = System.Drawing.Color.Snow;
+            this.C.Location = new System.Drawing.Point(93, 208);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(40, 75);
+            this.C.TabIndex = 15;
+            this.C.Text = "CE";
+            this.C.UseVisualStyleBackColor = false;
+            this.C.Click += new System.EventHandler(this.CE_Click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(244, 157);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(0, 0);
+            this.button16.TabIndex = 16;
+            this.button16.Text = "button16";
+            this.button16.UseVisualStyleBackColor = true;
+            // 
+            // button17
+            // 
+            this.button17.BackColor = System.Drawing.Color.DimGray;
+            this.button17.ForeColor = System.Drawing.Color.Snow;
+            this.button17.Location = new System.Drawing.Point(128, 208);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(40, 75);
+            this.button17.TabIndex = 17;
+            this.button17.Text = "←";
+            this.button17.UseVisualStyleBackColor = false;
+            this.button17.Click += new System.EventHandler(this.Radera_Click);
+            // 
+            // button18
+            // 
+            this.button18.BackColor = System.Drawing.Color.DimGray;
+            this.button18.ForeColor = System.Drawing.Color.Snow;
+            this.button18.Location = new System.Drawing.Point(12, 208);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(75, 75);
+            this.button18.TabIndex = 18;
+            this.button18.Text = "C";
+            this.button18.UseVisualStyleBackColor = false;
+            this.button18.Click += new System.EventHandler(this.C_Click);
+            // 
+            // button15
+            // 
+            this.button15.BackColor = System.Drawing.Color.DimGray;
+            this.button15.ForeColor = System.Drawing.Color.Snow;
+            this.button15.Location = new System.Drawing.Point(266, 208);
+            this.button15.Name = "button15";
+            this.button15.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button15.Size = new System.Drawing.Size(75, 75);
+            this.button15.TabIndex = 19;
+            this.button15.Text = "±";
+            this.button15.UseVisualStyleBackColor = false;
+            this.button15.Click += new System.EventHandler(this.reverse_Click);
+            // 
+            // button19
+            // 
+            this.button19.BackColor = System.Drawing.Color.DimGray;
+            this.button19.ForeColor = System.Drawing.Color.Snow;
+            this.button19.Location = new System.Drawing.Point(174, 207);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(75, 75);
+            this.button19.TabIndex = 20;
+            this.button19.Text = "√";
+            this.button19.UseVisualStyleBackColor = false;
+            this.button19.Click += new System.EventHandler(this.RotenUr_Click);
+            // 
+            // button20
+            // 
+            this.button20.BackColor = System.Drawing.Color.DimGray;
+            this.button20.ForeColor = System.Drawing.Color.Snow;
+            this.button20.Location = new System.Drawing.Point(174, 532);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(75, 75);
+            this.button20.TabIndex = 21;
+            this.button20.Text = ",";
+            this.button20.UseVisualStyleBackColor = false;
+            this.button20.Click += new System.EventHandler(this.comma_Click);
+            // 
+            // button21
+            // 
+            this.button21.BackColor = System.Drawing.Color.DimGray;
+            this.button21.ForeColor = System.Drawing.Color.Snow;
+            this.button21.Location = new System.Drawing.Point(12, 532);
+            this.button21.Name = "button21";
+            this.button21.Size = new System.Drawing.Size(75, 75);
+            this.button21.TabIndex = 22;
+            this.button21.Text = "1/x";
+            this.button21.UseVisualStyleBackColor = false;
+            this.button21.Click += new System.EventHandler(this.button21_Click);
+            // 
+            // Likamed
+            // 
+            this.Likamed.BackColor = System.Drawing.Color.DimGray;
+            this.Likamed.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.Likamed.FlatAppearance.BorderSize = 10;
+            this.Likamed.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Likamed.ForeColor = System.Drawing.Color.Snow;
+            this.Likamed.Location = new System.Drawing.Point(12, 126);
+            this.Likamed.Name = "Likamed";
+            this.Likamed.Size = new System.Drawing.Size(328, 75);
+            this.Likamed.TabIndex = 23;
+            this.Likamed.Text = "=";
+            this.Likamed.UseVisualStyleBackColor = false;
+            this.Likamed.Click += new System.EventHandler(this.likamed_Click);
+            this.Likamed.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Likamed_KeyDown);
+            // 
+            // Liten_Räknare
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(353, 619);
+            this.Controls.Add(this.Likamed);
+            this.Controls.Add(this.button21);
+            this.Controls.Add(this.button20);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.Resultat);
+            this.Controls.Add(this.Noll);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.Nio);
+            this.Controls.Add(this.Åtta);
+            this.Controls.Add(this.Sju);
+            this.Controls.Add(this.Sex);
+            this.Controls.Add(this.Fem);
+            this.Controls.Add(this.Fyra);
+            this.Controls.Add(this.Tre);
+            this.Controls.Add(this.Två);
+            this.Controls.Add(this.Ett);
+            this.MaximizeBox = false;
+            this.Name = "Liten_Räknare";
+            this.Text = "Liten_Räknare";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Ett;
+        private System.Windows.Forms.Button Två;
+        private System.Windows.Forms.Button Tre;
+        private System.Windows.Forms.Button Fyra;
+        private System.Windows.Forms.Button Fem;
+        private System.Windows.Forms.Button Sex;
+        private System.Windows.Forms.Button Sju;
+        private System.Windows.Forms.Button Åtta;
+        private System.Windows.Forms.Button Nio;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button Noll;
+        private System.Windows.Forms.TextBox Resultat;
+        private System.Windows.Forms.Button C;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button button20;
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.Button Likamed;
+    }
+}
+
---
+++ Litenräknare/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LitenRäknare
+{
+    public partial class Liten_Räknare : Form
+    {
+        Double värde = 0;
+        String symbol = "";
+        bool klickad_symbol = false;
+        bool passerad_symbol = false;
+        bool pressad_likamed = false;
+        private void button_Click(object sender, EventArgs e)
+        {
+            if ((klickad_symbol) || (Resultat.Text == "0"))
+                Resultat.Clear();
+
+            //Detta tillsammans med "boolen klickad symbol gör så att resultatrutan töms då du skriver något efter du klickat på någon av symbolerna som plus, minus osv...
+            klickad_symbol = false;
+            Button b = (Button)sender;
+            //Detta gör så att istället för att jag ger ett värde till knappen i koden genom "Resultat.Text + 1.Text kan jag bara ta värdet knappen fick redan under den grafiska processen så det inte behövs skrivas igen.
+
+            Resultat.Text = Resultat.Text + b.Text;
+        }
+
+        public Liten_Räknare()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Resultat_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CE_Click(object sender, EventArgs e)
+        {
+            Resultat.Clear();
+            Resultat.Text = "0";
+        }
+
+
+        private void symbol_Click(object sender, EventArgs e)
+        {
+            this.KeyPreview = true;
+            Button b = (Button)sender;
+            symbol = b.Text;
+
+            if (!klickad_symbol && passerad_symbol)
+            {
+                likamed();  //valde att göra en ny private void av likamed så att jag lätt kan länka denna funktionen och likamed knappens funktion till samma.
+            }
+            else
+            {
+                värde = Double.Parse(Resultat.Text);
+            }
+            klickad_symbol = true;
+            passerad_symbol = true;
+            // dessa hjälper mig att få en tom ruta varje gång jag klickar på ett räknetecken och att räkna ut resultatet av att klicka på ett räcknetecken tre gånger. 
+
+        }
+
+        private void likamed_Click(object sender, EventArgs e)
+        {
+            likamed();
+
+        }
+
+        private void C_Click(object sender, EventArgs e)
+        {
+            Resultat.Text = "0";
+            värde = 0;
+            klickad_symbol = false;
+            passerad_symbol = false;
+        }
+
+        private void Radera_Click(object sender, EventArgs e)
+        {
+            if (Resultat.Text.Length <= 0)
+            {
+                //Well.. den gör inget i detta fallet ju.
+            }
+            else if (Resultat.Text.Length > 0)
+            {
+                Resultat.Text = Resultat.Text.Remove(Resultat.Text.Length - 1);
+            }
+            //If satsen eftersom annars kraschar programmet ifall man försöker radera.
+        }
+
+        private void RotenUr_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            symbol = b.Text;
+            värde = Double.Parse(Resultat.Text);
+            klickad_symbol = true;
+            Resultat.Text = (Math.Pow(värde, 0.5)).ToString();
+            klickad_symbol = false;
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            if (Resultat.Text == "0")
+            {
+                Resultat.Text = "Gör inte en Anton nu..";
+            }
+            else
+            {
+                Button b = (Button)sender;
+                symbol = b.Text;
+                värde = Double.Parse(Resultat.Text);
+                klickad_symbol = true;
+                Resultat.Text = (1 / värde).ToString();
+                klickad_symbol = false;
+            }
+        }
+
+        private void comma_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            Resultat.Text = Resultat.Text + b.Text;
+
+        }
+
+        private void reverse_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            symbol = b.Text;
+            värde = Double.Parse(Resultat.Text);
+            klickad_symbol = true;
+            Resultat.Text = (Double.Parse(Resultat.Text) * -1).ToString();
+            klickad_symbol = false;
+
+        }
+        private void likamed()
+        {
+            if (pressad_likamed)
+            {
+                switch (symbol)
+                {
+                    case "+":
+                        Resultat.Text = (värde + Double.Parse(Resultat.Text)).ToString();
+                        break;
+                    case "-":
+                        Resultat.Text = (värde - Double.Parse(Resultat.Text)).ToString();
+                        break;
+                    case "x":
+                        Resultat.Text = (värde * Double.Parse(Resultat.Text)).ToString();
+                        break;
+                    case "/":
+                        Resultat.Text = (värde / Double.Parse(Resultat.Text)).ToString();
+                        break;
+
+                }
+            }
+            else
+            {
+                switch (symbol)
+                {
+                    case "+":
+                        Resultat.Text = (värde + Double.Parse(Resultat.Text)).ToString();
+                        break;
+                    case "-":
+                        Resultat.Text = (värde - Double.Parse(Resultat.Text)).ToString();
+                        break;
+                    case "x":
+                        Resultat.Text = (värde * Double.Parse(Resultat.Text)).ToString();
+                        break;
+                    case "/":
+                        Resultat.Text = (värde / Double.Parse(Resultat.Text)).ToString();
+                        break;
+
+
+                }
+                värde = Double.Parse(Resultat.Text);
+            }
+            //Finns kanske snyggare sätt att göra dett på men har denna if satsen så att när jag klickar =, = så tar den det senaste värdet jag skrev in och gångar det med resultatet.
+
+
+            klickad_symbol = false;
+            passerad_symbol = false;
+
+        }
+
+    
+    }
+}
